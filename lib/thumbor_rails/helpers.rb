7b3a46d068cadf7d62f3baf8b7d4e1b8628d1326
Rafael Caricio <rafael.jacinto@gmail.com>
2014-05-27 16:55:34 +0200

    Add thumbor_image_tag helper

--- lib/thumbor_rails/helpers.rb
+++ lib/thumbor_rails/helpers.rb
@@ -2,12 +2,17 @@ require 'ruby-thumbor'
 
 module ThumborRails
   module Helpers
+    include ActionView::Helpers::AssetTagHelper
 
     def thumbor_url(image_url, options = {})
       options[:image] = image_url
       thumbor_service = crypto_service
       thumbor_service = unsafe_service if options[:unsafe]
       "#{ThumborRails.server_url}#{thumbor_service.generate(options)}"
+    end
+
+    def thumbor_image_tag(image_url, options = {}, tag_attrs = {})
+      image_tag(thumbor_url(image_url, options), tag_attrs)
     end
 
     private
--- spec/thumbor_rails/helpers_spec.rb
+++ spec/thumbor_rails/helpers_spec.rb
@@ -3,15 +3,27 @@ require 'spec_helper'
 describe ThumborRails::Helpers do
   include ThumborRails::Helpers
 
-  it 'should generate a basic encrypted url' do
-    expect(thumbor_url('http://test.img')).to eq('http://thumbor.example.com/yPVDsGJsQKjKky_cdbkNuIpc9-A=/http://test.img')
+  describe "#thumbor_url" do
+    it 'should generate a basic encrypted url' do
+      expect(thumbor_url('http://test.img')).to eq('http://thumbor.example.com/yPVDsGJsQKjKky_cdbkNuIpc9-A=/http://test.img')
+    end
+
+    it 'should allow unsafe urls' do
+      expect(thumbor_url('http://test.img', unsafe: true)).to eq('http://thumbor.example.com/unsafe/http://test.img')
+    end
+
+    it 'should pass arguments to thumbor' do
+      expect(thumbor_url('http://test.img', unsafe: true, width: 100, height: 200)).to eq('http://thumbor.example.com/unsafe/100x200/http://test.img')
+    end
   end
 
-  it 'should allow unsafe urls' do
-    expect(thumbor_url('http://test.img', unsafe: true)).to eq('http://thumbor.example.com/unsafe/http://test.img')
-  end
+  describe '#thumbor_image_tag' do
+    it 'should return a simple image' do
+      expect(thumbor_image_tag('http://myimg.jpg')).to eq('<img alt="Myimg" src="http://thumbor.example.com/Q-1lWnxlCLnkzXWWM5xTAs1QEBM=/http://myimg.jpg" />')
+    end
 
-  it 'should pass arguments to thumbor' do
-    expect(thumbor_url('http://test.img', unsafe: true, width: 100, height: 200)).to eq('http://thumbor.example.com/unsafe/100x200/http://test.img')
+    it 'should pass arguments to thumbor' do
+      expect(thumbor_image_tag('http://myimg.jpg', unsafe: true)).to eq('<img alt="Myimg" src="http://thumbor.example.com/unsafe/http://myimg.jpg" />')
+    end
   end
 end
