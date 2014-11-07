8e59f9001269cd3fbd39cb2ae83d7f7fcc74f3ec
Rafael Caricio <rafael.jacinto@gmail.com>
2014-11-07 12:28:37 +0100

    Refactor in test suite, make everything more clear

--- spec/thumbor_rails/helpers_spec.rb
+++ spec/thumbor_rails/helpers_spec.rb
@@ -1,50 +1,77 @@
 require 'spec_helper'
 
 describe ThumborRails::Helpers do
+  let(:params) { { } }
+
   include ThumborRails::Helpers
 
-  describe "#thumbor_url" do
-    it 'should generate a basic encrypted url' do
-      expect(thumbor_url('http://test.img')).to eq('http://thumbor.example.com/yPVDsGJsQKjKky_cdbkNuIpc9-A=/http://test.img')
+  before do
+    ThumborRails.setup do |config|
+      config.security_key = 'MY_SECURITY_KEY'
+    end
+  end
+
+  describe 'interpolate thumbor hosts with %d to 0-3, like Rails asset_host' do
+    subject { thumbor_url('http://test.img') }
+    let!(:original_url) { ThumborRails.server_url }
+    before { ThumborRails.server_url = server_url }
+    after { ThumborRails.server_url = original_url }
+
+    context 'when url is set to "http://thumbor%d.com"' do
+      let(:server_url) { 'http://thumbor%d.com' }
+      it { should eq('http://thumbor3.com/yPVDsGJsQKjKky_cdbkNuIpc9-A=/http://test.img') }
+    end
+  end
+
+  describe 'automatically remove the protocol in source urls' do
+    subject { thumbor_url("#{protocol}test.img", unsafe: true) }
+    before { ThumborRails.force_no_protocol_in_source_url = true }
+    after { ThumborRails.force_no_protocol_in_source_url = false }
+
+    context 'when http' do
+      let(:protocol) { 'http://' }
+      it { should eq "http://thumbor.example.com/unsafe/test.img" }
     end
 
-    it 'should allow unsafe urls' do
-      expect(thumbor_url('http://test.img', unsafe: true)).to eq('http://thumbor.example.com/unsafe/http://test.img')
+    context 'when https' do
+      let(:protocol) { 'https://' }
+      it { should eq "http://thumbor.example.com/unsafe/test.img" }
     end
 
-    it 'should pass arguments to thumbor' do
-      expect(thumbor_url('http://test.img', unsafe: true, width: 100, height: 200)).to eq('http://thumbor.example.com/unsafe/100x200/http://test.img')
+    context 'when no protocol' do
+      let(:protocol) { 'www.' }
+      it { should eq "http://thumbor.example.com/unsafe/www.test.img" }
+    end
+  end
+
+  describe '#thumbor_url' do
+    subject { thumbor_url('http://test.img', params) }
+
+    context 'basic encrypted url' do
+      it { should eq('http://thumbor.example.com/yPVDsGJsQKjKky_cdbkNuIpc9-A=/http://test.img') }
     end
 
-    it 'should interpolate thumbor hosts with %d to 0-3, like Rails asset_host' do
-      original_url = ThumborRails.server_url
-      begin
-        ThumborRails.server_url = "http://thumbor%d.com"
-        expect(thumbor_url('http://test.img')).to eq('http://thumbor3.com/yPVDsGJsQKjKky_cdbkNuIpc9-A=/http://test.img')
-      ensure
-        ThumborRails.server_url = original_url
-      end
+    context 'allow unsafe urls' do
+      let(:params) { { unsafe: true } }
+      it { should eq('http://thumbor.example.com/unsafe/http://test.img') }
     end
 
-    it 'should automatically remove the protocol in source urls if configured to do so' do
-      begin
-        ThumborRails.force_no_protocol_in_source_url = true
-        expect(thumbor_url('http://test.img', unsafe: true)).to eq('http://thumbor.example.com/unsafe/test.img')
-        expect(thumbor_url('https://test.img', unsafe: true)).to eq('http://thumbor.example.com/unsafe/test.img')
-        expect(thumbor_url('www.test.img', unsafe: true)).to eq('http://thumbor.example.com/unsafe/www.test.img')
-      ensure
-        ThumborRails.force_no_protocol_in_source_url = false
-      end
+    context 'pass arguments to thumbor' do
+      let(:params) { { unsafe: true, width: 100, height: 200 } }
+      it { should eq('http://thumbor.example.com/unsafe/100x200/http://test.img') }
     end
   end
 
   describe '#thumbor_image_tag' do
-    it 'should return a simple image' do
-      expect(thumbor_image_tag('http://myimg.jpg')).to eq('<img alt="Myimg" src="http://thumbor.example.com/Q-1lWnxlCLnkzXWWM5xTAs1QEBM=/http://myimg.jpg" />')
+    subject { thumbor_image_tag('http://myimg.jpg', params) }
+
+    context 'unsafe disabled' do
+      it { should eq('<img alt="Myimg" src="http://thumbor.example.com/Q-1lWnxlCLnkzXWWM5xTAs1QEBM=/http://myimg.jpg" />') }
     end
 
-    it 'should pass arguments to thumbor' do
-      expect(thumbor_image_tag('http://myimg.jpg', unsafe: true)).to eq('<img alt="Myimg" src="http://thumbor.example.com/unsafe/http://myimg.jpg" />')
+    context 'unsafe enabled' do
+      let(:params) { { unsafe: true } }
+      it { should eq('<img alt="Myimg" src="http://thumbor.example.com/unsafe/http://myimg.jpg" />') }
     end
   end
 end
